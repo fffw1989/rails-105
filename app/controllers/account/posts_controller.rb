e32fa8706323c84b8566fcb174bf9bdebd7f244f
范威 <fffw1989@gmail.com>
2017-02-09 00:52:45 +0800

    implement My post/edit&delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,26 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:account_id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Post Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Post Deleted!'
+  end
 end
